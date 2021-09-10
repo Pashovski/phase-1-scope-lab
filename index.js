d77c38cb0dc57088ab6bc60aaac3102ea5f5c158
Pasha <pashdoctor@gmail.com>
2021-09-10 11:59:18 -0400

    commit tree f77ddff2cad903ca59fe5b3f8892332d2196acff

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+// function upperCaseCustomerName(){
+//     return customerName.toUpperCase()
+
+//     var customerName = "BOB"
+//     customerName.toUpperCase() 
+// }
+    
+function upperCaseCustomerName(){
+    // return customerName.toUpperCase()
+    
+    customerName = customerName.toUpperCase()
+    }
